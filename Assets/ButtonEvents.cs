f0f2edfc74d1f64ced03a78ca9fc87033ab29595
ItsAuro <zanizant@gmail.com>
2026-02-15 17:08:58 +0100

    Added scene switching logic
    - simple script to change scenes and quitting application

--- Assets/ButtonEvents.cs
+++ Assets/ButtonEvents.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DBG_SceneSwitcher : MonoBehaviour
+public class ButtonEvents : MonoBehaviour
 {
     public void SwitchScene(string targetScene)
     {   
         if (targetScene == null)
         {
-            Debug.LogWarning("Debug scene switcher has no target scene");
+            Debug.LogWarning("Scene switcher has no target scene");
             return;
         }
 
         SceneManager.LoadSceneAsync(targetScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
