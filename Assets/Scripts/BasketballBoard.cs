84bec745ea4490d4bc69be800ed0f111e12cd218
ItsAuro <zanizant@gmail.com>
2026-02-17 15:22:51 +0100

    Updated BasketballBoard to match application schema
    - updated to have bonus points not multiplier
    - updated rarity selection to use GameConfig

--- Assets/Scripts/BasketballBoard.cs
+++ Assets/Scripts/BasketballBoard.cs
@@ -5,63 +5,38 @@ using TMPro;
 using UnityEngine;
 
 
-enum Rarity 
-{ 
-    Common,
-    Uncommon,
-    Rare,
-    Epic,
-    Legendary
-}
+
 public class BasketballBoard : MonoBehaviour
 {
 
     [SerializeField]
-    TextMeshPro _TMP_multiplier;
+    TextMeshPro _TMP_bonus;
 
+    private int _rollFrequency = GameConfig.BACKBOARD_FREQUENCY;
+    public int PointBonus { get; private set;} = 1;
+
+
+    BonusRarity SelectRarity()
+    {
+        int gen = Random.Range(0, 100);
+        if (gen < GameConfig.RARITY_METADATA[BonusRarity.Common]  .threshold) return BonusRarity.Common;
+        if (gen < GameConfig.RARITY_METADATA[BonusRarity.Uncommon].threshold) return BonusRarity.Uncommon;
+        if (gen < GameConfig.RARITY_METADATA[BonusRarity.Rare]    .threshold) return BonusRarity.Rare;
+        if (gen < GameConfig.RARITY_METADATA[BonusRarity.Epic]    .threshold) return BonusRarity.Epic;
+        return BonusRarity.Legendary;
+    }
+    void UpdateText(BonusRarity rarity)
+    {
+        PointBonus = GameConfig.RARITY_METADATA[rarity].pointBonus;
+        _TMP_bonus.text = $"<color=#{ColorUtility.ToHtmlStringRGB(GameConfig.RARITY_METADATA[rarity].color)}>+{PointBonus}";
+    }
+    void SelectApplyRarity()
+    {
+        UpdateText(SelectRarity());
+    }
 
     void Start()
     {
-        _TMP_multiplier.text = $"<color=white>{1}x";
-
-        InvokeRepeating(nameof(SelectApplyRarity), 0, 5);
+        InvokeRepeating(nameof(SelectApplyRarity), 0, _rollFrequency);
     }
-
-    void SelectApplyRarity()
-    {
-        ApplyMultiplier(SelectRarity());
-    }
-
-    Rarity SelectRarity()
-    {
-        int gen = Random.Range(0, 100);
-        if (gen < 50) return Rarity.Common;
-        if (gen < 75) return Rarity.Uncommon;
-        if (gen < 87) return Rarity.Rare;
-        if (gen < 93) return Rarity.Epic;
-        return Rarity.Legendary;
-    }
-
-    void ApplyMultiplier(Rarity rarity)
-    {
-        switch (rarity) {
-            case Rarity.Common:
-                _TMP_multiplier.text = $"<color=white>{1}x";
-                break;
-            case Rarity.Uncommon:
-                _TMP_multiplier.text = $"<color=green>{2}x";
-                break;
-            case Rarity.Rare:
-                _TMP_multiplier.text = $"<color=blue>{4}x";
-                break;
-            case Rarity.Epic:
-                _TMP_multiplier.text = $"<color=purple>{8}x";
-                break;
-            case Rarity.Legendary:
-                _TMP_multiplier.text = $"<color=orange>{16}x";
-                break;
-        }
-    }
-
-
 }
--- Assets/Scripts/GameConfig.cs
+++ Assets/Scripts/GameConfig.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using System.Drawing;
+using UnityEngine;
 
 public enum BonusRarity
 {
@@ -18,25 +18,28 @@ public static class GameConfig
     public static readonly int FIREBALL_MULTIPLIER = 2;
     public static readonly int FIREBALL_INCREMENT = 1;
     public static readonly int FIREBALL_THRESHOLD = 100;
+    public static readonly int BACKBOARD_FREQUENCY = 5;
 
 
     public readonly struct RarityMetadata
     {
-        public RarityMetadata(Color c, int t)
+        public RarityMetadata(Color c, int t, int p)
         {
             color = c;
             threshold = t;
+            pointBonus = p;
         }
         public readonly Color color;
         public readonly int threshold;
+        public readonly int pointBonus;
     }
 
     public static readonly Dictionary<BonusRarity, RarityMetadata> RARITY_METADATA = new Dictionary<BonusRarity, RarityMetadata>
     {
-        {BonusRarity.Common,    new RarityMetadata(Color.White , 50)},
-        {BonusRarity.Uncommon,  new RarityMetadata(Color.Green , 75)},
-        {BonusRarity.Rare,      new RarityMetadata(Color.Blue  , 87)},
-        {BonusRarity.Epic,      new RarityMetadata(Color.Purple, 93)},
-        {BonusRarity.Legendary, new RarityMetadata(Color.Orange, 100)}
+        {BonusRarity.Common,    new RarityMetadata(Color.white ,  50,  1 )},
+        {BonusRarity.Uncommon,  new RarityMetadata(Color.green ,  75,  2 )},
+        {BonusRarity.Rare,      new RarityMetadata(Color.blue  ,  87,  4 )},
+        {BonusRarity.Epic,      new RarityMetadata(Color.magenta, 93,  8 )},
+        {BonusRarity.Legendary, new RarityMetadata(Color.yellow,  100, 16)}
     };
 }
