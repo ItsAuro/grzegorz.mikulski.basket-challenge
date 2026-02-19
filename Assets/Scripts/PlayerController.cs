84aca61634150dac07ae72e85e56e7e88ead3f64
ItsAuro <zanizant@gmail.com>
2026-02-19 15:40:14 +0100

    Fixed player jump and jitter
    player controller felt jittery due to multiple Move calls, combined them into one call

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float _movementSpeed = 5f;
     [SerializeField]
-    float _rotationSpeed = 20f;
+    float _rotationSpeed = .1f;
     [SerializeField]
     float _jumpForce     = 5f;
     [SerializeField]
@@ -37,22 +37,31 @@ public class PlayerController : MonoBehaviour
     {
         // convert XY input into XZ movement
         Vector3 movementDelta = transform.forward * movementVector.y + transform.right * movementVector.x;
-        movementDelta *= _movementSpeed * Time.deltaTime;
-        _characterController.Move(movementDelta);
+        movementDelta *= _movementSpeed;
+
+        if (_characterController.isGrounded && _velocityY < 0)
+            _velocityY = -2f;
+        _velocityY -= _gravity * Time.deltaTime;
+
+        movementDelta.y = _velocityY;
+        _characterController.Move(movementDelta * Time.deltaTime);
+
     }
     public void Look(Vector2 lookVector)
     {
         // X rotates body Y
-        transform.localRotation *= Quaternion.Euler(Vector3.up * _rotationSpeed * lookVector.x * Time.deltaTime);
+        transform.localRotation *= Quaternion.Euler(Vector3.up * _rotationSpeed * lookVector.x);
 
         //Y rotates head X
-        _headAngle = Mathf.Clamp(_headAngle + _rotationSpeed * lookVector.y * Time.deltaTime, -90, 90);
+        _headAngle = Mathf.Clamp(_headAngle + _rotationSpeed * lookVector.y, -90, 90);
         _head.transform.localRotation = Quaternion.Euler(Vector3.left * _headAngle);
     }
     public void Jump()
     {
-        if (!_characterController.isGrounded) return;
-        _velocityY = _jumpForce;
+        if (_characterController.isGrounded) 
+        {
+            _velocityY = _jumpForce;
+        }
     }
     public void ThrowBall()
     {
@@ -68,17 +77,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // character gravity pull down
-        if (!_characterController.isGrounded)
-        {
-            _velocityY -= _gravity * Time.deltaTime;
-        }
-        else 
-        {
-            _velocityY = -1f;
-        }
-
-        _characterController.Move(Vector3.up * _velocityY * Time.deltaTime);
-
+ 
     }
 }
